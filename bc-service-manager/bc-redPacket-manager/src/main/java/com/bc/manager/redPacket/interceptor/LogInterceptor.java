f65de17f7d70bce0cc0ac65b29db2bf78a022c4e
eaglezsx <577023309@qq.com>
2019-04-14 14:01:18 +0800

    gitHub第3次使用

--- bc-service-manager/bc-redPacket-manager/src/main/java/com/bc/manager/redPacket/interceptor/LogInterceptor.java
+++ bc-service-manager/bc-redPacket-manager/src/main/java/com/bc/manager/redPacket/interceptor/LogInterceptor.java
@@ -23,6 +23,5 @@ public class LogInterceptor extends HandlerInterceptorAdapter {
 		AuthToken userToken = XcTokenUtil.getUserToken(uid, redis);
 		log.info("IP:"+ IpUtil.getIpAddress(request)+" userName:"+userToken.getUsername()+" 动作："+request.getRequestURI());
 		return true;
-
 	}
 }
